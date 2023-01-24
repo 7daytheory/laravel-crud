95952f6e3f9fdb32f6fac4cb2a0e33381de8b393
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-01-24 18:43:45 -0500

    Created DB model, setup DB structure

--- app/Http/Controllers/ToDoController.php
+++ app/Http/Controllers/ToDoController.php
@@ -13,7 +13,7 @@ class ToDoController extends Controller
      */
     public function index()
     {
-        //
+        return 123;
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use Illuminate\Http\Controllers;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,8 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'ToDoController@index');
 
 Route::resource('todo', 'ToDoController');
