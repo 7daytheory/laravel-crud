7a9ae6ecc44a04b2e76e08a2de543d930d6b109e
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-01-26 15:18:46 -0500

    Setup sass files and create template

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
+            mix.js('resources/js/app.js', 'public/js')
+    .postCss('resources/sass/app.scss', 'public/css')
     ],
 });
