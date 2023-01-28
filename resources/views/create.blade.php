13839fd9dc7c23dba162772f3145f089729c2ad2
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-01-27 22:23:12 -0500

    Removed front-end required, added old values for value

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -7,15 +7,15 @@
     @csrf
   <div class="form-group">
     <label for="title">Title</label>
-    <input type="text" class="form-control" id="title" name="title" placeholder="Enter title" required>
+    <input type="text" class="form-control" id="title" name="title" placeholder="Enter title" value="{{ old('title') }}">
   </div>
   <div class="form-group">
     <label for="due">Due</label>
-    <input type="text" class="form-control" id="due" name="due" placeholder="Enter due date" required>
+    <input type="text" class="form-control" id="due" name="due" placeholder="Enter due date" value="{{ old('due') }}">
   </div>
   <div class="form-group">
     <label for="content">Content</label>
-    <textarea type="text" class="form-control" id="content" name="content" placeholder="Enter task description" required></textarea>
+    <textarea type="text" class="form-control" id="content" name="content" placeholder="Enter task description" value="{{ old('content') }}"></textarea>
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
